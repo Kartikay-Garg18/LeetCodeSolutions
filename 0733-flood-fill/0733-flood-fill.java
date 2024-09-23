ff3f8054809fe39acde3ffbf701f90b723f31406
Kartikay Garg <114226144+Kartikay-Garg18@users.noreply.github.com>
2024-09-23 19:19:04 +0530

    Time: 0 ms (100%), Space: 44.6 MB (83.69%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,35 +1,19 @@
 class Solution {
-    class Pair{
-        int row;
-        int col;
-        public Pair(int row, int col){
-            this.row = row;
-            this.col = col;
-        }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if(image[sr][sc]==color) return image;
+        int original = image[sr][sc];
+        fill(image,sr,sc,color,original);
+        return image;
     }
 
-    int[] rows = {1,-1,0,0};
-    int[] cols = {0,0,1,-1};
-
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int orig = image[sr][sc];
-        Queue<Pair> queue = new LinkedList<>();
-        queue.add(new Pair(sr, sc));
-        boolean[][] vis = new boolean[image.length][image[0].length];
-        while(!queue.isEmpty()){
-            Pair curr = queue.poll();
-            int r = curr.row;
-            int c = curr.col;
-            image[r][c] = color;
-            vis[r][c] = true;
-            for(int i=0; i<4; i++){
-                int nxtR = r + rows[i];
-                int nxtC = c + cols[i];
-                if(nxtR>=0 && nxtC>=0 && nxtR<image.length && nxtC<image[0].length && image[nxtR][nxtC] == orig && !vis[nxtR][nxtC]){
-                    queue.add(new Pair(nxtR, nxtC));
-                }
-            }
+    public void fill(int [][] image,int sr,int sc,int color,int orig){
+        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || image[sr][sc] != orig){
+            return;
         }
-        return image;
+        image[sr][sc] = color;
+        fill(image,sr+1,sc,color,orig);
+        fill(image,sr-1,sc,color,orig);
+        fill(image,sr,sc+1,color,orig);
+        fill(image,sr,sc-1,color,orig);
     }
 }
