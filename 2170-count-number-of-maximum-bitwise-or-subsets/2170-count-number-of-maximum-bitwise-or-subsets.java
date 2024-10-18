31673dd919daf4c8a846a406823cc001dca38dac
Kartikay Garg <114226144+Kartikay-Garg18@users.noreply.github.com>
2024-10-19 00:20:11 +0530

    Time: 9 ms (67.71%), Space: 41.3 MB (46.49%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
@@ -1,28 +1,20 @@
 class Solution {
 
-    HashSet<List<Integer>> list = new HashSet<>();
+    public int findOr(int[] nums, int idx, int or, int max){
+       
+        if(idx>= nums.length) return max==or?1:0;
 
-    public void findOr(int[] nums, int idx, int or, int[] max, List<Integer> ans){
-        if(or>max[0]){
-            max[0] = or;
-            list.clear();
-            list.add(new ArrayList<>(ans));
-        } else if(or==max[0]){
-            list.add(new ArrayList<>(ans));
-        }
-        
-        if(idx>= nums.length) return;
+        return findOr(nums, idx+1, or, max) + findOr(nums, idx+1, or|nums[idx], max);
 
-        findOr(nums, idx+1, or, max, ans);
-        ans.add(idx);
-        findOr(nums, idx+1, or|nums[idx], max, ans);
-        ans.remove(ans.size()-1);
     }
 
     public int countMaxOrSubsets(int[] nums) {
-        int[] maxOr = {0};
         int n = nums.length;
-        findOr(nums, 0, 0, maxOr, new ArrayList<>());
-        return list.size();
+        int max = 0;
+
+        for(int ele : nums) max|=ele;
+
+        return findOr(nums, 0, 0, max);
+        
     }
 }
