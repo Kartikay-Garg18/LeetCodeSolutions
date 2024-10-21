5bbf96a47ce0dd7340c10127631d529c54e81e2b
Kartikay Garg <114226144+Kartikay-Garg18@users.noreply.github.com>
2024-10-21 08:37:07 +0530

    Time: 2 ms (76.33%), Space: 61.5 MB (72.37%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,28 +1,28 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        // int profit = 0;
-        // int min = prices[0];
-        // for(int i=1; i<prices.length; i++){
-        //     int cost = prices[i] - min;
-        //     profit = profit>cost?profit:cost;
-        //     min = prices[i]<min?prices[i]:min;
-        // }
-        // return profit;
-        int n = prices.length;
-        int[] buy = new int[n];
-        buy[0] = prices[0];
-        for(int i=1; i<n; i++){
-            buy[i] = Math.min(buy[i-1], prices[i]);
-        }
-        int[] sell = new int[n];
-        sell[n-1] = prices[n-1];
-        for(int i=n-2; i>=0; i--){
-            sell[i] = Math.max(prices[i], sell[i+1]);
-        }
         int profit = 0;
-        for(int i=0; i<n; i++){
-            profit = Math.max(profit, sell[i] - buy[i]);
+        int min = prices[0];
+        for(int i=1; i<prices.length; i++){
+            int cost = prices[i] - min;
+            profit = profit>cost?profit:cost;
+            min = prices[i]<min?prices[i]:min;
         }
         return profit;
+        // int n = prices.length;
+        // int[] buy = new int[n];
+        // buy[0] = prices[0];
+        // for(int i=1; i<n; i++){
+        //     buy[i] = Math.min(buy[i-1], prices[i]);
+        // }
+        // int[] sell = new int[n];
+        // sell[n-1] = prices[n-1];
+        // for(int i=n-2; i>=0; i--){
+        //     sell[i] = Math.max(prices[i], sell[i+1]);
+        // }
+        // int profit = 0;
+        // for(int i=0; i<n; i++){
+        //     profit = Math.max(profit, sell[i] - buy[i]);
+        // }
+        // return profit;
     }
 }
