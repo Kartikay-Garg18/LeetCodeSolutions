7638f555d8b84b4475f18104c31677af137f5f9f
Kartikay Garg <114226144+Kartikay-Garg18@users.noreply.github.com>
2024-10-23 08:51:10 +0530

    Time: 75 ms (16.95%), Space: 80.2 MB (39.83%) - LeetHub

---
+++ 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public void update(List<TreeNode> list, int sum, HashMap<TreeNode, Integer> map){
+        for(TreeNode curr : list){
+            int feed = sum-map.get(curr);
+            if(curr.left!=null) curr.left.val = feed;
+            if(curr.right!=null) curr.right.val = feed;
+        }
+    }
+    
+    public TreeNode replaceValueInTree(TreeNode root) {
+        HashMap<TreeNode,Integer> map = new HashMap<>();
+        int childSum = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        root.val = 0;
+        queue.add(root);
+        queue.add(null);
+        List<TreeNode> list = new ArrayList<>();
+        while(!queue.isEmpty()){
+            TreeNode curr = queue.poll();
+            if(curr!=null){
+                int a = curr.left!=null?curr.left.val:0;
+                int b = curr.right!=null?curr.right.val:0;
+                map.put(curr, a+b);
+                childSum+=a+b;
+                if(curr.left!=null) queue.add(curr.left);
+                if(curr.right!=null) queue.add(curr.right);
+                list.add(curr);
+            } else{
+                update(list, childSum, map);
+                childSum=0;
+                list.clear();
+                if(queue.isEmpty()) break;
+                else{
+                    queue.add(null);
+                }
+            }
+        }
+        return root;
+    }
+}
