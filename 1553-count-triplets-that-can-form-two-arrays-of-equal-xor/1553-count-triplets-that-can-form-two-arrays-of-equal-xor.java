da3a7e01b25dd42303d6cfa6c1b2e408ca067d69
Kartikay Garg <114226144+Kartikay-Garg18@users.noreply.github.com>
2024-11-05 10:37:45 +0530

    Time: 3 ms (25.21%), Space: 41.3 MB (38.23%) - LeetHub

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/1553-count-triplets-that-can-form-two-arrays-of-equal-xor.java
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/1553-count-triplets-that-can-form-two-arrays-of-equal-xor.java
@@ -7,14 +7,27 @@ class Solution {
 
         for(int i = 1; i<=n; i++) prefix[i] ^= prefix[i-1];
 
+        // for(int i=0; i<=n; i++){
+        //     for(int j = i+1; j<=n; j++){
+        //         if(prefix[i] == prefix[j]){
+        //             cnt+=(j-i-1);
+        //         }
+        //     }
+        // }
+
+        HashMap<Integer, Integer> countMap = new HashMap<>();
+        HashMap<Integer, Integer> totalSum = new HashMap<>();
+        countMap.put(0, 0);
+        totalSum.put(0, 0);
         for(int i=0; i<=n; i++){
-            for(int j = i+1; j<=n; j++){
-                if(prefix[i] == prefix[j]){
-                    cnt+=(j-i-1);
-                }
+            int xor = prefix[i];
+            if(countMap.containsKey(xor)){
+                cnt += (countMap.get(xor) * (i-1) - totalSum.get(xor));
             }
+            countMap.put(xor, countMap.getOrDefault(xor, 0)+1);
+            totalSum.put(xor, totalSum.getOrDefault(xor, 0)+i);
         }
-        
+
         return cnt;
     }
 }
