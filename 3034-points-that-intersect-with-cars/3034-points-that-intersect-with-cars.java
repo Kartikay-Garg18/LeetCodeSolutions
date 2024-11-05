7191d9e0cc390b3ff389525dac8d3e627b53125f
Kartikay Garg <114226144+Kartikay-Garg18@users.noreply.github.com>
2024-11-05 14:51:51 +0530

    Time: 1 ms (100%), Space: 44.5 MB (36.1%) - LeetHub

--- 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.java
+++ 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.java
@@ -1,11 +1,11 @@
 class Solution {
     public int numberOfPoints(List<List<Integer>> nums) {
         boolean[] vis = new boolean[101];
+        
         for(List<Integer> ele : nums){
-            for(int i = ele.get(0); i<=ele.get(1); i++){
-                vis[i] = true;
-            }
+            Arrays.fill(vis, ele.get(0), ele.get(1)+1, true);
         }
+
         int cnt = 0;
         for(boolean flag : vis) if(flag) cnt++;
         return cnt;
