d71790b135a8d5323a659e50eba008755e9b2c5c
Kartikay Garg <114226144+Kartikay-Garg18@users.noreply.github.com>
2024-11-06 18:54:27 +0530

    Time: 3 ms (98.09%), Space: 59.9 MB (49.51%) - LeetHub

--- 1206-corporate-flight-bookings/1206-corporate-flight-bookings.java
+++ 1206-corporate-flight-bookings/1206-corporate-flight-bookings.java
@@ -2,10 +2,10 @@ class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
         int[] ans = new int[n];
         for(int[] ele : bookings){
-            for(int i=ele[0]-1; i<ele[1]; i++){
-                ans[i]+=ele[2];
-            }
+            ans[ele[0]-1]+=ele[2];
+            if(ele[1]<n) ans[ele[1]]-=ele[2];
         }
+        for(int i=1; i<n; i++) ans[i]+=ans[i-1];
         return ans;
     }
 }
