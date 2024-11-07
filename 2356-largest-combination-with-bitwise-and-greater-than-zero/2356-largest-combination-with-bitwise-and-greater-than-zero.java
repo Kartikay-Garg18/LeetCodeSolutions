f340fb1d20e0fac496b8903ab9a9a2221c92c268
Kartikay Garg <114226144+Kartikay-Garg18@users.noreply.github.com>
2024-11-07 08:43:05 +0530

    Time: 26 ms (95.24%), Space: 56.4 MB (72.91%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.java
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.java
@@ -1,19 +1,21 @@
 class Solution {
     public int largestCombination(int[] candidates) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int[] map = new int[32];
         for(int ele : candidates){
-            int bit = 1;
+            int bit = 0;
             while(ele>0){
                 int set = ele&1;
                 if(set==1){
-                    map.put(bit, map.getOrDefault(bit, 0)+1);
+                    map[bit]++;
                 }
                 ele>>=1;
                 bit++;
             }
         }
+
         int max = 1;
-        for(int ele : map.values()){
+        for(int ele : map){
             if(ele>max) max = ele;
         }
         return max;
