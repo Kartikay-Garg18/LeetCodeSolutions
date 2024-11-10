9ed9f44db39ddd2389bc6bece169ecb654647061
Kartikay Garg <114226144+Kartikay-Garg18@users.noreply.github.com>
2024-11-10 19:35:43 +0530

    Time: 12 ms (88.01%), Space: 41.9 MB (97.43%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.java
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.java
@@ -8,63 +8,47 @@
  * }
  */
 class Solution {
-    class Pair{
-        TreeNode root;
-        int dist;
-        public Pair(TreeNode root, int dist){
-            this.root = root;
-            this.dist = dist;
-        }
-    }
+    List<Integer> list = new ArrayList<>();
 
-    public void build(HashMap<Integer, List<TreeNode>> graph, TreeNode root){
-        if(root == null) return;
-        if(!graph.containsKey(root.val)){
-            graph.put(root.val, new ArrayList<>());
+    private void printKDown(TreeNode root, int k){
+        if(root == null || k<0) return;
+        if(k== 0){
+            list.add(root.val);
+            return;
         }
-        if(root.left!= null && !graph.containsKey(root.left.val)){
-            graph.put(root.left.val, new ArrayList<>());
+        printKDown(root.left, k-1);
+        printKDown(root.right, k-1);
+    }
+    
+    private int printKFar(TreeNode root, TreeNode target, int k){
+        if(root == null) return -1;
+        if(root == target){
+            printKDown(root, k);
+            return 0;
         }
-        if(root.right!= null && !graph.containsKey(root.right.val)){
-            graph.put(root.right.val, new ArrayList<>());
+        int dl = printKFar(root.left, target, k);
+        if(dl != -1){
+            if(dl+1 == k){
+                list.add(root.val);
+            } else{
+                printKDown(root.right, k - dl - 2);
+            }
+            return 1+dl;
         }
-        if(root.left!=null){
-            graph.get(root.val).add(root.left);
-            graph.get(root.left.val).add(root);
+        int dr = printKFar(root.right, target, k);
+        if(dr!=-1){
+            if(dr+1 == k){
+                list.add(root.val);
+            } else{
+                printKDown(root.left, k - dr - 2);
+            }
+            return 1+dr;
         }
-        if(root.right!=null){
-            graph.get(root.val).add(root.right);
-            graph.get(root.right.val).add(root);
-        }
-        build(graph, root.left);
-        build(graph, root.right);
+        return -1;
     }
 
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        HashMap<Integer, List<TreeNode>> graph = new HashMap<>();
-        List<TreeNode> vis = new ArrayList<>();
-        List<Integer> ans = new ArrayList<>();
-        // visited.add(target);
-        if(k==0){
-            ans.add(target.val);
-            return ans;
-        }
-        build(graph, root);
-        Queue<Pair> queue = new LinkedList<>();
-        queue.add(new Pair(target, 0));
-        while(!queue.isEmpty()){
-            Pair curr = queue.poll();
-            vis.add(curr.root);
-            for(TreeNode neigh : graph.get(curr.root.val)){
-                int nxtDist = curr.dist+1;
-                if(nxtDist == k && !vis.contains(neigh)){
-                    ans.add(neigh.val);
-                }
-                if(!vis.contains(neigh)){
-                    queue.add(new Pair(neigh, nxtDist));
-                }
-            }
-        }
-        return ans;
+        printKFar(root, target, k);
+        return list;
     }
 }
