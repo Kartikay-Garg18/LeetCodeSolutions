ac166e8991715eadb7a9922111cbecd5ae111483
Kartikay Garg <114226144+Kartikay-Garg18@users.noreply.github.com>
2024-11-11 10:55:59 +0530

    Time: 222 ms (5.26%), Space: 58.2 MB (6.62%) - LeetHub

---
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.java
@@ -0,0 +1,50 @@
+class Solution {
+    boolean[] prime;
+    Boolean[][] dp;
+    private void primeSeive(int n){
+        for(int i=2; i*i<=n; i++){
+            if(prime[i]){
+                for(int p = i*i; p<=n; p+=i){
+                    prime[p] = false;
+                }
+            }
+        }
+    }
+
+    private boolean isSorted(int[] nums, int idx, int p){
+
+        if(idx == nums.length) return true;
+
+        if(dp[idx][p] != null) return dp[idx][p];
+
+        int num = nums[idx];
+        if(p<num){
+            if(isSorted(nums, idx+1, num)) return dp[idx][p] = true;
+        }
+
+        for(int i=num-1; i>=2; i--){
+            if(prime[i] && num-i>p){
+                if(isSorted(nums, idx+1, num-i)) return dp[idx][p] = true;
+            }
+        }
+
+        return dp[idx][p] = false;
+    }
+
+    public boolean primeSubOperation(int[] nums) {
+        int n = nums.length;
+        if(n==1) return true;
+        int max = -1;
+        for(int ele : nums)
+            if(max<ele) max = ele;
+
+        dp = new Boolean[n][max+1];
+        prime = new boolean[max+1];
+        Arrays.fill(prime, true);
+        primeSeive(max);
+        
+        return isSorted(nums, 0, 0);
+        // int parent = 0;
+        // for(int i)
+    }
+}
