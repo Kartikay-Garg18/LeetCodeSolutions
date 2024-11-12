cd152d48316fd91e128254e4065709148e9c497e
Kartikay Garg <114226144+Kartikay-Garg18@users.noreply.github.com>
2024-11-12 22:18:37 +0530

    Time: 0 ms (100%), Space: 41.5 MB (39.3%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,14 +1,7 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length() != goal.length()) return false;
-
-        int n = s.length();
-
-        for(int i=0; i<n; i++){
-            String form = s.substring(i)+s.substring(0,i);
-            if(form.equals(goal)) return true;
-        }
-
-        return false;
+        if(goal.length()<s.length()) return false;
+        s+=s;
+        return s.contains(goal);
     }
 }
