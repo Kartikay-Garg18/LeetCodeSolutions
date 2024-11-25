e4eb404244a28d97ad3cd63c702af08a1f284306
Kartikay Garg <114226144+Kartikay-Garg18@users.noreply.github.com>
2024-11-25 08:42:43 +0530

    Time: 0 ms (100%), Space: 41.4 MB (82.13%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -3,16 +3,17 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int[][] dp = new int[m][n];
-        boolean obs = false;
+
         for(int i=0; i<n; i++){
-            if(grid[0][i] == 1) obs = true;
-            dp[0][i] = obs?0:1;
+            if(grid[0][i] == 1) break;
+            dp[0][i] = 1;
         }
-        obs = false;
+
         for(int i=0; i<m; i++){
-            if(grid[i][0] == 1) obs = true;
-            dp[i][0] = obs?0:1;
+            if(grid[i][0] == 1) break;
+            dp[i][0] = 1;
         }
+
         for(int i=1; i<m; i++){
             for(int j=1; j<n; j++){
                 if(grid[i][j] == 1){
@@ -22,6 +23,7 @@ class Solution {
                 }
             }
         }
+        
         return dp[m-1][n-1];
     }
 }
