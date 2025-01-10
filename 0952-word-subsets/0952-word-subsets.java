5d7cdb765b472c761959cfcd182d6c488f1683dc
Kartikay Garg <114226144+Kartikay-Garg18@users.noreply.github.com>
2025-01-10 09:46:54 +0530

    Time: 13 ms (81.4%), Space: 54.7 MB (74.42%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+        int[] uni = new int[26];
+        for(String word : words2){
+            int[] temp = new int[26];
+            for(char ch : word.toCharArray()){
+                temp[ch-'a']++;
+            }
+            for(int i=0; i<26; i++){
+                uni[i] = Math.max(uni[i], temp[i]);
+            }
+        }
+        
+        List<String> ans = new ArrayList<>();
+        for(String word : words1){
+            int[] curr = new int[26];
+            for(char ch : word.toCharArray()){
+                curr[ch-'a']++;
+            }
+
+            boolean flag = true;
+            for(int i=0; i<26; i++){
+                if(uni[i]>curr[i]){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) ans.add(word);
+        }
+
+        return ans;
+    }
+}
